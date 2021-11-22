bfdb86699385badc3b810f3a5189c97be07db7c0
“mayuri” <mayurisalunkhe01@gmail.com>
2021-11-22 10:08:21 +0530

    Adding Cab Invoice Generator to remote

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to Cab Invoice Generator..!");
+            Console.ReadLine();
+        }
+    }
+}
